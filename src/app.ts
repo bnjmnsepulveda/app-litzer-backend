dc497fa09bbf8154c6813ba34f87ee836cc40dac
Benjamin Sepulveda <benjamin.sepulveda@witi.cl>
2021-04-10 18:44:16 -0400

    configuring basic server

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from "type-graphql";
+import { AlbumResolver } from './graphql/resolvers/AlbumResolver';
+import Express from "express";
+
+const PORT = process.env.PORT || 5000;
+
+async function bootstrap() {
+    // ... Building schema here
+    const schema = await buildSchema({
+        resolvers: [AlbumResolver]
+    });
+    // Create the GraphQL server
+    const server = new ApolloServer({
+        schema,
+        playground: true,
+    });
+
+    const app = Express();
+    server.applyMiddleware({ app });
+    app.listen({ port: PORT }, () => console.log(`🚀 Server ready and listening at ==> http://localhost:${PORT}${server.graphqlPath}`))
+
+}
+
+
+bootstrap();
