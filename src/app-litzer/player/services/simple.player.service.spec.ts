5ff21cc24f245e75cd92822cc80740dac75a3b6e
Benjamin Sepulveda <benjamin.sepulveda@witi.cl>
2021-04-24 14:29:09 -0400

    fix tests

--- src/app-litzer/player/services/simple.player.service.spec.ts
+++ src/app-litzer/player/services/simple.player.service.spec.ts
@@ -5,7 +5,6 @@ test('create a player', async () => {
     const service = new SimplePlayerService()
     const created = await service.create('player-test')
     expect(created).toBeDefined()
-    expect(service.players.length).toEqual(1)
 })
 
 test('find player by id', async () => {
@@ -29,5 +28,5 @@ test('add song to player', async () => {
     }
     const songAdded = await service.addSong(addSong)
     expect(songAdded.id).toEqual('1')
-    expect(service.players[0].queque.length).toEqual(1)
+    expect(service.players[1].queque.length).toEqual(1)
 })
